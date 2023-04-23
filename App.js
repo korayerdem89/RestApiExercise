71909d07b19e1ba2809af2f80e43a2c8602404fc
Koray Erdem KILIÇ <developerkoray@gmail.com>
2023-04-23 12:20:23 +0300

    stack screen eklendi

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Episodes from './src/Episodes';
-
+import { NavigationContainer } from '@react-navigation/native';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
     <Stack.Navigator>
     <Stack.Screen name="Episodes" component={Episodes} />
   </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
